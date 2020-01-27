38e481ae8c52a8990bcfcdb050b023aa0b09c421
Deep Gaurav <deepgauravraj@gmail.com>
2020-01-27 22:17:52 +0530

    Add ping

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 use ws::{listen, Sender, Handler, Handshake, CloseCode, Message};
 use crate::process_shell::ProcessShell;
 use ws::util::Token;
+//use serde::{Serialize,Deserialize};
+use serde::{Serialize,Deserialize};
 
 mod process_shell;
 
+#[derive(Serialize,Deserialize,Debug)]
+struct TransferData{
+    command:String,
+    value:String,
+    args:Vec<String>
+}
+
+const PING :Token = Token(1);
+const READ_PROCESS_OUT:Token = Token(2);
+
 struct Server{
     out:Sender,
-    shell:ProcessShell
+    shell:Vec<ProcessShell>
 }
 impl Handler for Server{
 
@@ -21,33 +33,43 @@ impl Handler for Server{
     }
 
     fn on_message(&mut self, msg: Message) -> ws::Result<()> {
-        let data =msg.into_data();
-        println!("Shell write {:?}",data);
-        self.shell.write(&data);
+//        println!("receive msg ");
+        if let Ok(string_msg) = String::from_utf8(msg.into_data()){
+            match serde_json::from_str::<TransferData>(&string_msg){
+                Ok(data)=>{
+                    if data.command=="ping"{
+                        if let Some(time)= data.args.get(0){
+                            if let Ok(time)=time.parse::<i64>(){
+                                let now_time = chrono::Utc::now().timestamp_millis();
+                                println!("connection id {} : ping one way time : {}",self.out.connection_id(),now_time-time);
+                                let pong = TransferData{
+                                    command: "ping".to_string(),
+                                    value: data.value,
+                                    args: vec![format!("{}",now_time)]
+                                };
+                                if let Ok(sendstr)=serde_json::to_string(&pong){
+                                    self.out.send(Message::from(sendstr));
+                                }
+                            }
+                        }
+                    }
+                }
+                Err(err)=>{
+                    println!("Error {}",err);
+                }
+            }
+        }
         Ok(())
     }
 
     fn on_close(&mut self, code: CloseCode, reason: &str) {
-        self.shell.kill();
+        for mut process in &mut self.shell{
+            process.kill();
+        }
         println!("Connection closing due to ({:?}) {}, connection_id {} ", code, reason,self.out.connection_id());
     }
 
     fn on_timeout(&mut self, event: Token) -> ws::Result<()> {
-        match event{
-            Token(1) => {
-                self.out.timeout(100,Token(1));
-
-                let data = self.shell.read();
-                if !data.is_empty(){
-                    println!("Shell read {:?}", String::from_utf8(data.clone()));
-                    self.out.ping(data.clone());
-                    self.out.send(Message::from(String::from_utf8(data).unwrap()));
-                }
-            },
-            _ => {
-
-            }
-        }
         Ok(())
     }
 
@@ -59,7 +81,7 @@ fn main(){
     let addr = format!("{}:{}",std::env::var("HOST").unwrap_or("0.0.0.0".to_owned()),std::env::var("PORT").unwrap_or("3012".to_owned()));
     println!("Running on address {} ",addr);
     if let Err(error) = listen(&addr, |out| {
-        Server{out,shell:ProcessShell::new()}}) {
+        Server{out,shell:Vec::new()}}) {
         // Inform the user of failure
         println!("Failed to create WebSocket due to {:?}", error);
     }
