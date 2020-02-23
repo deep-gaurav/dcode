25a670ca070d136d7c5f04b8d12fa67b9e8c2bcc
Deep Gaurav <deepgauravraj@gmail.com>
2020-02-24 01:40:53 +0530

    increase terminal size

--- src/process_shell.rs
+++ src/process_shell.rs
@@ -18,7 +18,7 @@ impl ProcessShell{
         match pty_system {
             Ok(pty_system)=>{
                 let pair = pty_system.openpty(PtySize{
-                    rows:200,
+                    rows:2000,
                     cols:800,
                     pixel_height:0,
                     pixel_width:0
@@ -37,7 +37,7 @@ impl ProcessShell{
                                             child,
                                             pair,
                                             stdout: out,
-                                            vt100:Parser::new(24,80,0),
+                                            vt100:Parser::new(2000,800,0),
                                             last_content:None
                                         })
                                     },
